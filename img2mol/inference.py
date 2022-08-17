5799a8fe12f7abb0869aad4126679bb2fe5485cb
Otto Brinkhaus <brinkhaus@gmail.com>
2022-08-17 11:12:06 +0200

    local CDDD option + avoid boilerplate code for user

--- img2mol/inference.py
+++ img2mol/inference.py
@@ -12,19 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+sys.path = sys.path[1:]
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 import torch
 from torchvision import transforms
 
-from rdkit import Chem
-
+import os
 from typing import Optional
 import random
 import numpy as np
-
 from PIL import Image, ImageOps, ImageEnhance
 
 from img2mol.model import Img2MolPlModel
 from img2mol.cddd_server import CDDDRequest
+
+from cddd.inference import InferenceModel as CDDDInferenceModel
+
+from rdkit import Chem
+
+
 
 """
 Inference Class for Img2Mol Model.
@@ -39,14 +48,36 @@ class Img2MolInference(object):
     """
     Inference Class
     """
-    def __init__(self, model_ckpt: Optional[str] = None, device: str = "cpu"):
+    def __init__(
+        self,
+        model_ckpt: Optional[str] = None,
+        device: str = "cuda:0" if torch.cuda.is_available() else "cpu",
+        local_cddd: bool = False
+    ):
         super(Img2MolInference, self).__init__()
+        if local_cddd:
+            self.cddd_inference_model = CDDDInferenceModel()
+        else:
+            self.cddd_inference_model = False
+            self.cddd_server = CDDDRequest()
         self.device = device
         print("Initializing Img2Mol Model with random weights.")
         self.model = Img2MolPlModel()
         if model_ckpt is not None:
             print(f"Loading checkpoint: {model_ckpt}")
             self.model = self.model.load_from_checkpoint(model_ckpt)
+        else:
+            # Model path in package_dir
+            model_path = os.path.join(os.path.split(__file__)[0],
+                                      'model',
+                                      'model.ckpt')
+            if os.path.exists(model_path):
+                print(f"Loading checkpoint: {model_path}")
+                self.model = self.model.load_from_checkpoint(model_path)
+            else:
+                raise FileNotFoundError(
+                    "1", "Please download the img2mol model from",
+                    f"https://drive.google.com/file/d/1pk21r4Zzb9ZJkszJwP9SObTlfTaRMMtF/view and save it at {model_path}")
 
         print("Setting to `self.eval()`-mode.")
         self.model.eval()
@@ -105,13 +136,13 @@ class Img2MolInference(object):
         if not extension:
             return "Image must be jpg or png format!"
         image = self.read_imagefile(filepath)
-        images = torch.cat([torch.unsqueeze(self.transform_image(image), 0) for _ in range(repeats)], dim=0)
+        images = torch.cat([torch.unsqueeze(self.transform_image(image), 0)
+                            for _ in range(repeats)], dim=0)
         images = images.to(self.device)
-
         return images
 
-    def __call__(self, filepath: str,
-                 cddd_server: CDDDRequest,
+    def __call__(self,
+                 filepath: str,
                  return_cddd: bool = False,
                  ) -> dict:
         images = self.read_image_to_tensor(filepath, repeats=50)
@@ -121,7 +152,10 @@ class Img2MolInference(object):
         # take the median cddd prediction out of `repeats` predictions
         cddd = np.median(cddd, axis=0)
 
-        smiles = cddd_server.cddd_to_smiles(cddd.tolist())
+        if self.cddd_inference_model:
+            smiles = self.cddd_inference_model.emb_to_seq(cddd)
+        else:
+            smiles = self.cddd_server.cddd_to_smiles(cddd.tolist())
         mol = Chem.MolFromSmiles(smiles, sanitize=True)
         # if the molecule is valid, i.e. can be parsed with the rdkit
         if mol:
@@ -139,12 +173,10 @@ class Img2MolInference(object):
                 "cddd": cddd, "smiles": can_smiles, "mol": can_mol
                 }
 
-
     def predict(self, filepath: str,
                 cddd_server: CDDDRequest,
                 return_cddd: bool = False) -> dict:
         return self.__call__(filepath, cddd_server, return_cddd)
-
 
 
 if __name__ == "__main__":
@@ -152,7 +184,6 @@ if __name__ == "__main__":
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     img2mol = Img2MolInference(model_ckpt=None,
                                device=device)
-
     cddd_server = CDDDRequest(host="http://ec2-18-157-240-87.eu-central-1.compute.amazonaws.com")
 
     example = "examples/example1.png"
