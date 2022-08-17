4bd05b4ed93a5827a2e6443d87f1a94fa4fb88e4
Otto Brinkhaus <brinkhaus@gmail.com>
2022-08-17 12:58:49 +0200

    delete unnecessary sys.path modificaiton

--- img2mol/inference.py
+++ img2mol/inference.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-sys.path = sys.path[1:]
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
